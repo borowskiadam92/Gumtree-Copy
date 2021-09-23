6c0715fb8d0118bb8cfd77610c242da5af10328c
Patryk <patryk.marcisz.v2@gmail.com>
2021-09-23 11:58:16 +0200

    modyfikacje stylizacji

--- gumtree-front/src/app/pages/main-page/category-item/category-item.component.ts
+++ gumtree-front/src/app/pages/main-page/category-item/category-item.component.ts
@@ -3,8 +3,7 @@ import {Category} from "../../../shared/model/category/category";
 
 @Component({
   selector: 'app-category-item',
-  templateUrl: './category-item.component.html',
-  styleUrls: ['./category-item.component.scss']
+  templateUrl: './category-item.component.html'
 })
 export class CategoryItemComponent implements OnInit {
 
--- gumtree-front/src/app/shared/directive/dropdown.directive.ts
+++ gumtree-front/src/app/shared/directive/dropdown.directive.ts
@@ -1,13 +1,16 @@
-import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.show') isShow = false;
 
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    console.log(event);
+    console.log(this.elRef.nativeElement.contains(event.target));
+    console.log(this.elRef.nativeElement);
+    this.isShow = this.elRef.nativeElement.contains(event.target) ? !this.isShow : false;
   }
 
   constructor(private elRef: ElementRef) {
